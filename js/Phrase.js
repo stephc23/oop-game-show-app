d75a94faeb3ac7d0cb15e4dd3ec79a484acedfd9
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-01-07 15:08:42 -0500

    Created addPhraseToDisplay method

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,4 +7,20 @@ class Phrase {
         this.phrase = phrase.toLowerCase();
     }
 
+    /**
+     * Adds letter placeholders to the display.
+     */
+    addPhraseToDisplay() {
+        for (let char of this.phrase) {
+            const characterLi = document.createElement('li');
+            const phraseDiv = document.querySelector('#phrase');
+            phraseDiv.firstElementChild.appendChild(characterLi);
+
+            if (char = ' ') {
+                characterLi.setAttribute('class', 'space');
+            } else {
+                characterLi.setAttribute('class', `hide letter ${char}`);
+            }
+        } 
+    }
 }
