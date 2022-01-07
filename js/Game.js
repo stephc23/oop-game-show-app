32f5142a17356d037ee17a3ee8b571f82fdd013d
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-01-07 18:20:31 -0500

    Added startGame, getRandomPhrase, and handleInteraction methods

--- js/Game.js
+++ js/Game.js
@@ -14,4 +14,46 @@ class Game {
         ];
         this.activePhrase = null;
     }
+
+    /**
+     * Hides the start screen overlay, sets the activePhrase property to a random phrase, and adds the phrase to the board.
+     */
+    startGame() {
+        document.querySelector('#overlay').style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    /**
+     * Randomly retrieves one of the phrases stored in the phrases array.
+     * @returns {object} The random Phrase object.
+     */
+    getRandomPhrase() {
+        const randomIndex = Math.ceil(Math.random * 4);
+        return this.phrases[randomIndex];
+    }
+
+    /**
+     * Branches code, depending on if selected letter is in the phrase. Checks if game has been won.
+     * @param {object} event - Click event object.
+     */
+    handleInteraction(event) {
+        const keyButton = event.target;
+        keyButton.disabled = true;
+
+        const selectedLetter = keyButton.textContent;
+        const isInPhrase = this.activePhrase.checkLetter(selectedLetter);
+        if (isInPhrase) {
+            keyButton.classList.add('chosen');
+            this.activePhrase.showMatchedLetter(selectedLetter);
+        } else {
+            keyButton.classList.add('wrong');
+            this.removeLife();
+        }
+
+        const hasWon = this.checkForWin();
+        if (hasWon) {
+            this.gameOver();
+        }
+    }
 }
--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const game = new Game();
+
+const keyboardDiv = document.querySelector('#qwerty');
+keyboardDiv.addEventListener('click', event => {
+    game.handleInteraction(event);
+});
