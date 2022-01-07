0461afa423ff96fe02d11ffcf5ffe403cef57b38
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-01-07 15:48:57 -0500

    Added checkLetter and ShowMatchedLetter methods

--- js/Phrase.js
+++ js/Phrase.js
@@ -11,7 +11,7 @@ class Phrase {
      * Adds letter placeholders to the display.
      */
     addPhraseToDisplay() {
-        for (let char of this.phrase) {
+        for (const char of this.phrase) {
             const characterLi = document.createElement('li');
             const phraseDiv = document.querySelector('#phrase');
             phraseDiv.firstElementChild.appendChild(characterLi);
@@ -22,5 +22,32 @@ class Phrase {
                 characterLi.setAttribute('class', `hide letter ${char}`);
             }
         } 
+    }
+
+    /**
+     * Checks if the letter selected by the player matches a letter in the phrase.
+     * @param {string} selected - The letter selected by the player.
+     * @returns {boolean} True if the selected letter matches a letter in the phrase; false if it matches no letters.
+     */
+    checkLetter(selected) {
+        for (const char of this.phrase) {
+            if (char === selected) {
+                return true;
+            } 
+        }
+        return false;
+    }
+
+    /**
+     * Shows letters on the board that matches the player's selected letter.
+     * @param {string} matched - The players selected letter that matches at least one letter in the phrase.
+     */    
+    showMatchedLetter(matched) {
+        const letterLis = document.querySelector('#phrase').firstElementChild.children;
+        for (const li of letterLis) {
+            if (li.classList.contains(matched)) {
+                li.classList.replace('hide', 'show');
+            }
+        }
     }
 }
