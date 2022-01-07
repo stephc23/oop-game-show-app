54942734fdd18084d1f7d410b6f9c0e5200c7b0c
stephc23 <88057766+stephc23@users.noreply.github.com>
2022-01-07 17:37:42 -0500

    Created Game class and constructor

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,17 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = [
+            new Phrase('Happy Holidays'),
+            new Phrase('Merry Christmas'),
+            new Phrase('Happy New Year'),
+            new Phrase('Let it snow'),
+            new Phrase('Jingle bells')
+        ];
+        this.activePhrase = null;
+    }
+}
